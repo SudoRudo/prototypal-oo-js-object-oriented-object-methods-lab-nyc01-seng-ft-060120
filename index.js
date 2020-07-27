06a04d1768cf641467321e7f08c306185ba9c000
Rodolfo  Guerrero-Martinez II <r.guerreronyc@gmail.com>
2020-07-27 04:50:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function BoardMember(name, homestate, training){
+  this.name = name;
+  this.homestate = homestate;
+  this.training = training;
+}
+
+BoardMember.prototype.veto = function() {
+ return "" 
+}
